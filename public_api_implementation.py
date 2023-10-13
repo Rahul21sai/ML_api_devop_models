379fbfd3c8f8db055fd6289ae2014b277772dd56
VUDUMULA NAGA SAI RAHUL <110412514+Rahul21sai@users.noreply.github.com>
2023-10-14 00:21:26 +0530

    Add files via upload

---
+++ public_api_implementation.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 14 00:06:20 2023
+
+@author: nagas
+"""
+
+import json
+import requests
+
+url = 'https://0cc2-34-125-194-246.ngrok.io/diabetes_prediction'
+
+input_data_for_model ={
+    
+    
+    'Pregnancies' : 6,
+    'Glucose'  : 148,
+    'BloodPressure' : 72,
+    'SkinThickness' : 35,
+    'Insulin' : 0,
+    'BMI' : 33.6,
+    'DiabetesPedigreeFunction' : 0.627,
+    'Age' : 50
+        
+    }
+    
+
+input_json = json.dumps(input_data_for_model)
+
+response = requests.post(url, data=input_json)
+
+print(response.text)
